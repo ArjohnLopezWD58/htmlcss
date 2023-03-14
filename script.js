72f6cac0879024eda74320426538b5ffc1fbf9a9
ArjohnLopezwd58 <amiemimo2124@gmail.com>
2023-03-13 21:55:42 -0700

    added

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+const list = document.querySelectorAll('.lst');
+function activelink(){
+    list.forEach((item)) =>
+    item.classlist.remove('active');
+    this.classlist.add(active);
+}
+list.forEach((item) =>
+item.addEventlistener('click',))
+</script>
